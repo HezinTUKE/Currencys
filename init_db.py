3bb586ebe1f1cf9ec0d79772c233958e3ce4cd15
HezinTUKE <vova.gezin@gmail.com>
2024-06-07 01:50:19 +0200

    Create Database

--- init_db.py
+++ init_db.py
@@ -1,11 +1,20 @@
 from sqlalchemy import create_engine, MetaData
+from sqlalchemy_utils import database_exists, create_database
 from app.models import _currency
+from settings import config
 
 # URL for connecting to Postgres DB with engine.
-DB_URL = "postgresql://{user}:{pswrd}@{host}:{port}/{db_name}".format(
-    user="postgres", pswrd="admin", host="localhost",
-    port="5432", db_name="test_currency"
+DB_URL = "postgresql://{user}:{password}@{host}:{port}/{database}".format(
+    **config['postgres']
 )
+
+if not database_exists(DB_URL):
+    create_database(DB_URL)
+
+if database_exists(DB_URL):
+    print("DATABASE was created")
+else:
+    print("DATABASE was not created")
 
 engine = create_engine(DB_URL, isolation_level='AUTOCOMMIT')
 
