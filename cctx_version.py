6a0fc1b63a0a67269e0f7fcff4877a846cd90eea
HezinTUKE <vova.gezin@gmail.com>
2024-06-12 14:45:00 +0200

    rm

--- cctx_version.py
+++
@@ -1,9 +0,0 @@
-import os
-import ccxt.static_dependencies.toolz as toolz
-
-d = os.path.dirname(toolz.__file__)
-version_ = os.path.join(d, '__init__.py')
-
-if os.path.exists(version_):
-    print("exists")
-    os.remove(toolz.__file__)
